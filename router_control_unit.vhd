2539a06a657b0a14ad08a7d4d46acc52b5df6d84
mirkoggl <mirko.gagliardi@gmail.com>
2015-11-10 21:00:57 +0100

    aa

--- router_control_unit.vhd
+++ router_control_unit.vhd
@@ -50,97 +50,96 @@ use work.logpack.all;
 use work.routerpack.all;
 
 entity router_control_unit is
-	Generic (
+	Generic(
 		LOCAL_X : natural := 1;
 		LOCAL_Y : natural := 1
 	);
-	Port (
-		clk   : in std_logic;
-		reset : in std_logic;
-		
-		Data_II  : in data_array_type;							  -- Data input from all the Input Interfaces
-		Empty_II : in std_logic_vector(CHAN_NUMBER-1 downto 0);	  -- Empty FIFO control signal from all the Input Interfaces
-		Full_OI  : in std_logic_vector(CHAN_NUMBER-1 downto 0);   -- Full FIFO control signal from all the Output Interfaces
-		
-		Shft_II   : out std_logic_vector(CHAN_NUMBER-1 downto 0); -- Shift enable signal to Input Interfaces
-		Wr_En_OI  : out std_logic_vector(CHAN_NUMBER-1 downto 0); -- Write enable signal to Output Interfaces
-		Cross_Sel : out crossbar_sel_type						  -- Crossbar sel control signal	
+	Port(
+		clk       : in  std_logic;
+		reset     : in  std_logic;
+
+		Data_II   : in  data_array_type; -- Data input from all the Input Interfaces
+		Empty_II  : in  std_logic_vector(CHAN_NUMBER - 1 downto 0); -- Empty FIFO control signal from all the Input Interfaces
+		Full_OI   : in  std_logic_vector(CHAN_NUMBER - 1 downto 0); -- Full FIFO control signal from all the Output Interfaces
+
+		Shft_II   : out std_logic_vector(CHAN_NUMBER - 1 downto 0); -- Shift enable signal to Input Interfaces
+		Wr_En_OI  : out std_logic_vector(CHAN_NUMBER - 1 downto 0); -- Write enable signal to Output Interfaces
+		Cross_Sel : out crossbar_sel_type -- Crossbar sel control signal	
 	);
 end entity router_control_unit;
 
 architecture RTL of router_control_unit is
-		
 	COMPONENT routing_logic_xy
 		Generic(
-			LOCAL_X    : natural := 1;
-			LOCAL_Y    : natural := 1
+			LOCAL_X : natural := 1;
+			LOCAL_Y : natural := 1
 		);
 		Port(
-			Data_In      : in std_logic_vector(DATA_WIDTH-1 downto 0);
-			In_Channel   : in std_logic_vector(SEL_WIDTH-1 downto 0);
-			Out_Channel  : out std_logic_vector(SEL_WIDTH-1 downto 0); 
-			Crossbar_Sel : out crossbar_sel_type		
+			Data_In      : in  std_logic_vector(DATA_WIDTH - 1 downto 0);
+			In_Channel   : in  std_logic_vector(SEL_WIDTH - 1 downto 0);
+			Out_Channel  : out std_logic_vector(SEL_WIDTH - 1 downto 0);
+			Crossbar_Sel : out crossbar_sel_type
 		);
 	END COMPONENT routing_logic_xy;
-	
+
 	COMPONENT rr_arbiter
-		Port (
-			Counter_In : in std_logic_vector(SEL_WIDTH - 1 downto 0);
-			Valid_In   : in std_logic_vector(CHAN_NUMBER - 1 downto 0);
+		Port(
+			Counter_In : in  std_logic_vector(SEL_WIDTH - 1 downto 0);
+			Valid_In   : in  std_logic_vector(CHAN_NUMBER - 1 downto 0);
 			Win_OneHot : out std_logic_vector(CHAN_NUMBER - 1 downto 0);
 			Win_Out    : out std_logic_vector(SEL_WIDTH - 1 downto 0)
 		);
 	END COMPONENT;
-	
+
 	COMPONENT onehot_encoder
 		Port(
 			vect_in  : in  std_logic_vector(SEL_WIDTH - 1 downto 0);
 			vect_out : out std_logic_vector(CHAN_NUMBER - 1 downto 0)
 		);
 	end component onehot_encoder;
-	
+
 	constant ONE_VECT : std_logic_vector(CHAN_NUMBER - 1 downto 0) := (others => '1');
-	type state_type is (out_wren, out_delay); 
-	
+	type state_type is (idle, out_delay);
+
 	-- Control Unit Signals
-	signal current_s : state_type := out_wren;
+	signal current_s   : state_type                                 := idle;
 	signal n_empty_in  : std_logic_vector(CHAN_NUMBER - 1 downto 0) := (others => '0');
-	signal rr_counter  : std_logic_vector(SEL_WIDTH - 1 downto 0) := (others => '0');
-	signal rr_index    : std_logic_vector(SEL_WIDTH - 1 downto 0)  := (others => '0');
-	signal xy_data_in  : std_logic_vector(DATA_WIDTH - 1 downto 0) := (others => '0');
-	signal xy_chan_in  : std_logic_vector(SEL_WIDTH - 1 downto 0) := (others => '0');
-	signal xy_chan_out : std_logic_vector(SEL_WIDTH - 1 downto 0) := (others => '0');
-	
+	signal rr_counter  : std_logic_vector(SEL_WIDTH - 1 downto 0)   := (others => '0');
+	signal rr_index    : std_logic_vector(SEL_WIDTH - 1 downto 0)   := (others => '0');
+	signal xy_data_in  : std_logic_vector(DATA_WIDTH - 1 downto 0)  := (others => '0');
+	signal xy_chan_in  : std_logic_vector(SEL_WIDTH - 1 downto 0)   := (others => '0');
+	signal xy_chan_out : std_logic_vector(SEL_WIDTH - 1 downto 0)   := (others => '0');
+
 	signal fifo_input  : std_logic_vector(CHAN_NUMBER - 1 downto 0) := (others => '0');
 	signal fifo_output : std_logic_vector(CHAN_NUMBER - 1 downto 0) := (others => '0');
-	
+
 begin
 
-  -----------------------------------------------------------------------
-  -- Round Robin Selector
-  -----------------------------------------------------------------------
-		
+	-----------------------------------------------------------------------
+	-- Round Robin Selector
+	-----------------------------------------------------------------------
+
 	rr_arb_inst : rr_arbiter
 		Port Map(
 			Counter_In => rr_counter,
-			Valid_In => n_empty_in,
+			Valid_In   => n_empty_in,
 			Win_OneHot => fifo_input,
-			Win_Out  => rr_index
+			Win_Out    => rr_index
 		);
-	
+
 	n_empty_in <= not Empty_II;
 
-  -----------------------------------------------------------------------
-  -- DOR Routing Logic
-  -----------------------------------------------------------------------
-  	
-  	xy_data_in <= Data_II(CONV_INTEGER(rr_index)) when Empty_II /= ONE_VECT; 
-	xy_chan_in <= rr_index when Empty_II /= ONE_VECT; 	
-  	
+	-----------------------------------------------------------------------
+	-- DOR Routing Logic
+	-----------------------------------------------------------------------
+
+	xy_data_in <= Data_II(CONV_INTEGER(rr_index)) when Empty_II /= ONE_VECT;
+	xy_chan_in <= rr_index when Empty_II /= ONE_VECT;
+
 	XY_logic : routing_logic_xy
 		Generic Map(
-			LOCAL_X    => LOCAL_X,
-			LOCAL_Y    => LOCAL_Y
+			LOCAL_X => LOCAL_X,
+			LOCAL_Y => LOCAL_Y
 		)
 		Port Map(
 			Data_In      => xy_data_in,
@@ -148,58 +147,56 @@ begin
 			Out_Channel  => xy_chan_out,
 			Crossbar_Sel => Cross_Sel
 		);
-		
---	onehot_enc : onehot_encoder
---		Port Map(
---			vect_in  => xy_chan_out,
---			vect_out => fifo_output
---		);
-	
---	Wr_En_OI <= fifo_output when Empty_II /= ONE_VECT else (others => '0');
---  Shft_II <= fifo_input when Empty_II /= ONE_VECT else (others => '0');
-	
-	
-  -----------------------------------------------------------------------
-  -- Control Unit
-  -----------------------------------------------------------------------	
-  
-	CU_process : process (clk, reset)
+
+	--	onehot_enc : onehot_encoder
+	--		Port Map(
+	--			vect_in  => xy_chan_out,
+	--			vect_out => fifo_output
+	--		);
+
+	--	Wr_En_OI <= fifo_output when Empty_II /= ONE_VECT else (others => '0');
+	--  Shft_II <= fifo_input when Empty_II /= ONE_VECT else (others => '0');
+
+
+	-----------------------------------------------------------------------
+	-- Control Unit
+	-----------------------------------------------------------------------	
+
+	CU_process : process(clk, reset)
 	begin
 		if reset = '1' then
-			current_s <= out_wren;
-			Shft_II <= (others => '0');
-			Wr_En_OI <= (others => '0');
+			current_s  <= idle;
+			Shft_II    <= (others => '0');
+			Wr_En_OI   <= (others => '0');
 			rr_counter <= (others => '0');
-		
-		elsif rising_edge(clk) then		
-			
-			Shft_II <= (others => '0');
-			Wr_En_OI <= (others => '0');			
-			
-			if rr_counter = CONV_STD_LOGIC_VECTOR(CHAN_NUMBER-1, SEL_WIDTH) then
+
+		elsif rising_edge(clk) then
+			Shft_II  <= (others => '0');
+			Wr_En_OI <= (others => '0');
+
+			if rr_counter = CONV_STD_LOGIC_VECTOR(CHAN_NUMBER - 1, SEL_WIDTH) then
 				rr_counter <= (others => '0');
 			else
 				rr_counter <= rr_counter + '1';
 			end if;
-					
-			case current_s is
 
-			when out_wren =>
-			 if Empty_II /= ONE_VECT then	
-				if Full_OI(CONV_INTEGER(xy_chan_out)) = '1' then  -- FIFO Out full, scarta il pacchetto e torna idle
-					current_s <= out_wren;
-				else
-					current_s <= out_delay;
-					Wr_En_OI(CONV_INTEGER(xy_chan_out)) <= '1';
-					Shft_II(CONV_INTEGER(xy_chan_in)) <= '1';
-				end if;
-			 end if;
-			
-			when out_delay => 	-- Stato usato per generare impulsi di write ed evitare di scrivere nel buffer di uscita più volte lo stesso dato
-				current_s <= out_wren;	    
+			case current_s is
+				when idle =>
+					if Empty_II /= ONE_VECT then
+						if Full_OI(CONV_INTEGER(xy_chan_out)) = '1' then -- FIFO Out full, scarta il pacchetto e torna idle
+							current_s <= idle;
+						else
+							current_s                           <= out_delay;
+							Wr_En_OI(CONV_INTEGER(xy_chan_out)) <= '1';
+							Shft_II(CONV_INTEGER(xy_chan_in))   <= '1';
+						end if;
+					end if;
+
+				when out_delay =>       -- Stato usato per generare impulsi di write ed evitare di scrivere nel buffer di uscita più volte lo stesso dato
+					current_s <= idle;
 			end case;
-		
+
 		end if;
 	end process;
-	
+
 end architecture RTL;
